1f5445be33eb46eae89ac848fb78c3a9f5e955af
riot <riot@hackerfleet.org>
2012-10-29 21:44:11 +0100

    Last tiny manual refactoring fixes
    --HG--
    rename : CONFIGS/riot.conf => CONFIGS/ms0x00.conf
    rename : Tools/ANRVCLIClient.py => Tools/RAINCLIClient.py
    rename : Tools/ANRVGUICLIClient.py => Tools/RAINGUICLIClient.py
    rename : Tools/ANRVJSONClient.py => Tools/RAINJSONClient.py
    rename : Tools/ANRVMonitor.py => Tools/RAINMonitor.py
    rename : Tools/ANRVVis.py => Tools/RAINVis.py

--- RAIN/__init__.py
+++ RAIN/__init__.py
@@ -1,14 +1,15 @@
-"""ANRV - Autonomous Naval Robotic Vehicle Software Package
+"""RAIN alpha
 
-This is the top module of the ANRV system.
+This is the top module of the RAIN system.
 It contains the following packages:
 
+* Autonomy      - Behavioural components
 * Communication - Components handling external communication
 * Controls      - Actuators and other controllable components
-* Interface     - The various user interfaces to control ANRVs
+* Interface     - The various user interfaces to control RAINs
 * Sensors       - Measuring components
-* System        - ANRV internal components
+* System        - RAIN system components
 
-For more Information, see https://hackerfleet.org/pydoc 
+For more Information, see https://hackerfleet.org/pydoc
 or contact riot@hackerfleet.org.
 """
