e24cad220b021dcb7d4808d1ae0ab875e735d2d2
riot <riot@hackerfleet.org>
2012-12-06 21:14:41 +0100

    Ex-Scaffold rain.py documented via comments

--- rain.py
+++ rain.py
@@ -2,14 +2,30 @@
 
 import sys
 
+#
+# First: Load Configuration
+#
+
 import RAIN.System.Configuration as Configuration
 Configuration.setupConfig()
+
+#
+# Second: Setup the Logger
+#
+
 from RAIN.System import Logger as Logger
 Logger.setupLogger()
+
+#
+# Third: The node's Identity
+#
 
 from RAIN.System import Identity as Identity
 Identity.setupIdentity()
 
+#
+# Fourth: Initialize and inspect all components
+#
 Logger.systeminfo("-"*42)
 Logger.systeminfo("RAIN booting.")
 
@@ -19,6 +35,9 @@ from RAIN.Controls import *
 from RAIN.Sensors import *
 from RAIN.Autonomy import *
 
+#
+# Fifth: Construct a base system and instantiate the Node
+#
 
 import RAIN.System.Dispatcher as Dispatcher
 import RAIN.System.Registry as Registry
@@ -32,6 +51,7 @@ from Kamaelia.Internet.TCPClient import TCPClient
 from Kamaelia.Chassis.Pipeline import Pipeline
 from Kamaelia.Util.Introspector import Introspector
 
+# Kamaelia-style introspection; Due to change.
 port = 55555
 introspector = False
 
@@ -61,12 +81,18 @@ dispatcher.RegisterComponent(registrycomponent)
 Registry.Components[dispatcher.name] = dispatcher
 Registry.Components[registrycomponent.name] = registrycomponent
 
+Logger.systeminfo("Initializing Node from Configuration.")
 registrycomponent.initFromConfig()
 
+# JSON gateway; Due to change in favour of AMQP node2node communication
 Logger.systeminfo("Setting up JSONServer on port %i" % port)
 jsonserver = ServerCore(protocol=JSONProtocol, port=port)
 jsonserver.activate()
 dispatcher.RegisterComponent(jsonserver)
+
+#
+# Sixth: Run the assembled RAIN Node
+#
 
 if ServerConfig['runserver']:
     Logger.systeminfo("Starting all threads")
@@ -74,3 +100,7 @@ if ServerConfig['runserver']:
 else:
     Logger.systeminfo("Thats it. Terminating.")
     sys.exit(0)
+
+#
+# Fin
+#
